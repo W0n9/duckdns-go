27ef93a4620c05d3ef199d22382d965b6df14824
Eldwan Brianne <eldwan35@gmail.com>
2021-01-11 19:12:55 +0100

    Added go structure

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ebrianne/duckdns-go
+
+go 1.15
+
+// require (
+// 
+// )
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/ebrianne/duckdns-go/internal"
+)
+
+const (
+	name = "duckdns-go"
+)
+
+func main() {
+
+}
